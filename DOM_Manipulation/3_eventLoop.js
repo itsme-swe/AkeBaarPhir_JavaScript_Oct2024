b4aec6e2e2a1715cd26f01003002dcb73e58cf6f
itsme-swe <me.harshmehra07@gmail.com>
2024-10-01 16:41:11 +0530

    update

--- DOM_Manipulation/3_eventLoop.js
+++ DOM_Manipulation/3_eventLoop.js
@@ -1,3 +1,19 @@
 /*
+1️⃣ Measure Speed of code ⇒ To measure the spped of our code we use " performance.now() " method. This return us an timestamp of our code.
 
+2️⃣ To optimize the code we should make sure that we should avoid more reflow and repaint.
+
+3️⃣ 📌 Document Fragment ⇒ It is light weight document object. We use document fragement to avoid re-flow and repaint bcoz reflow and repaint is highly extensive task. 
+
+🔸 Example of single reflow and single repaint by using document fragement.
+  
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i <= 100; i++){
+        let newElement = document.createElement('p');
+        newElement.textContent = `This is para ${i}`;
+
+        fragment.appendChild(newElement);
+    }
+    
+    document.body.appendChild(fragment);
 */
