5564a87753806eb90bde0c899599f531f82a4a2c
itsme-swe <me.harshmehra07@gmail.com>
2024-10-01 08:19:43 +0530

    New Repo

---
+++ DOM_Manipulation/1_introDOM.js
@@ -0,0 +1,60 @@
+/*
+💥  DOM Intro ⇒ PART ONE
+
+1️⃣ DOM : Process of converting HTML code into JS object is known as DOM.
+
+2️⃣ BOM : Browser Object Model help to communicate to browser other than content. E.g alerts
+
+3️⃣ To Access an Elements we use:
+
+    1. document.getElementById : returns a single object.
+
+    2. document.getElementsByClassName and document.getElementsByTagName : returns a list of objects and it's an HTML collection.
+
+    3. document.querySelector() : It returns the single object. We can pass class or id as parameter inside querySelector.
+
+    4. document.querySelectorAll('.hidden') : It return the multiple objects and the list is of type node list.
+
+4️⃣ Updating Existing Content of Window using few properties ⇒
+
+    1. .innerHTML : get and set the HTML content of any element and its desendents.
+
+    2. .outerHTML :
+
+    3. .textContent : get and set the actual text based content of the element.
+
+    4. .innerText : get and set the actual text based content of the element. Remember jis kisi ki bhi display property hidden hogi vo "innerText" pae render nhi hoga.
+
+5️⃣ Create Element using JS : To create element using JS we use " .createElement() ". Now after creating we want to add this element, so to add element we use " content.appendChild() ".
+
+6️⃣ If we create an new element and adding that element to some parent or child that element will be the last sibling of that element. If we want to position newly created elements according to us, we can use " .insertAdjacentHTML() method ". 
+
+    🔸 .insertAdjacentHTML() method itself take two parametrs inside it as follows:
+
+        1. location /position ⇒ means where element to be added. There are four positionional properties :
+          a. beforeBegin
+          b. afterBegin
+          c. beforeEnd
+          d. afterEnd
+
+        2. HTML text /content to be inserted ⇒ means what we want to insert
+
+7️⃣ Styling our page content using JS ⇒ We can add CSS to our elements by using JS properties are as follow:
+
+    🔸  .style  : is property ko use karke hum 1 baar mae 1 he CSS property add kar sukte hai.
+    
+    🔸  .cssText : is property ka use karke hum multiple css properties add kar sukte hai single line mae.
+                
+              content.style.cssText = 'color: green; background-color: yellow; font-size: 10px';
+
+    🔸  .setAttribute : is property ko hum sirf style kae liye he nhi balki element mae baki attributes kae lie bhi   use kar sukte hai. Jaise " content.setAttribute("id", "idName") " , content.setAttribute("style",)
+    
+    🔸  .className : return karta hai string of classes
+
+    🔸 .classList : return karta hai humae array of classes.
+
+*/
+
+const newChild = document.querySelector(".child");
+
+newChild.setAttribute("id", "big-child");
---
+++ DOM_Manipulation/2_browserEventsDOM.js
@@ -0,0 +1,49 @@
+/*
+💥 Browser Events ⇒ DOM PART TWO
+
+1️⃣ monitorEvents() method is used to see the events occured on mouse click events, keyboard events and many more.
+
+  Example: monitorEvents(document)
+
+  🔸 Is example pae hum monitorEvents() method ko document kae upper use kar rahe hai, aub joh bhi events agar document pae hongae toh hum unhae dekh sakta hai.
+
+2️⃣ EventTarget : Yae ake top-level interface hota hai joh ki objects kae dwara use kiya jaata hai, kisi bhi events ko receive karne kae liye aur unpae listeners laganae kae liye. Eventtarget kae 3 method hotae hai:
+
+  1. addEventListeners()
+
+  2. removeeventListener()
+  
+  3. dispatchEvent()
+
+3️⃣ EventTarget ⇦⇦⇦ Node : Node inherit all the properties and methods from EventTarget.
+
+4️⃣ EvenTarget ⇦⇦⇦ Node ⇦⇦⇦ Elements : And, elements inherit all the properties from node.
+
+5️⃣ To apply .addEventListener() method we require 3 things:
+    1. <event-target>   E.g. <div>, <article>, <p>
+    2. "event-type"   E.g. "click", "scroll", "hover"
+    3. function   E.g function will tell what to be performed after event happen.
+
+    🔸 Syntax ⇒ <event-target>.addEventListener("click", () => {});
+
+6️⃣ Phases of Events ⇒
+
+    1. Capturing : First phase where event starts at the root of the DOM tree and move downward towards the target. If we pass "true" as parameter inside addEventListener() it means we are enabling capturing phase.
+    
+      E.g : element.addEventListener('click', callback, true); // 'true' enables capturing
+
+    2. Target Phase : When the event reaches the actual target.
+
+    3. Bubling Phase : After handling the event in this phase target move again towards the root of the DOM tree.
+
+    Event phases: 
+      🔸Capture: Root ⇢⇢ Parent Elemnt ⇢⇢ Target
+
+      🔸 Target: Event handled on the target phase.
+
+      🔸 Bubbling: Taget ⇢⇢ Parent Element ⇢⇢ Root
+
+7️⃣ To stop the by default behaviour of any tag we use " .preventDefault( ) ".  
+
+8️⃣9️⃣🔟
+*/
